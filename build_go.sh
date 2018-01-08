83712d5ffdc591fd0d42b090f2a6ac17c8df2f69
Rajarshi Banerjee <rajarshi.banerjee1@synechron.com>
2018-01-08 13:34:04 +0000

    Update build_go.sh

--- build_go.sh
+++ build_go.sh
@@ -1,5 +1,7 @@
-cd /root/quorum-maker/NodeManagerGo
-go build
-mv /root/quorum-maker/NodeManagerGo/NodeManagerGo /root/quorum-maker/.
+cd /usr/local/go/src/synechron.com/NodeManagerGo
+go build 
+mv /usr/local/go/src/synechron.com/NodeManagerGo/NodeManagerGo /root/quorum-maker/.
+cd /usr/local/go/src/
+rm -rf github.com 
+rm -rf synechron.com
 cd /root/quorum-maker
-rm -rf NodeManagerGo
