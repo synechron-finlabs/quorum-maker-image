a0a857a53a28f940d51cc8c192a82a57780b613d
Rajarshi Banerjee <rajarshi.banerjee1@synechron.com>
2018-01-10 09:01:09 +0000

    Update build_go.sh

--- build_go.sh
+++ build_go.sh
@@ -1,10 +1,7 @@
 go get -u github.com/gorilla/mux
 go get -u github.com/ybbus/jsonrpc
 go get -u github.com/magiconair/properties
-mkdir /root/go/src/synechron.com
-cd /root/go/src/synechron.com/
-git clone https://RakhiB:Rakhi123@gitlab.com/quorum-maker/NodeManagerGo.git
-cd NodeManagerGo
+cd /root/go/src/synechron.com/NodeManagerGo
 go build
 mv /root/go/src/synechron.com/NodeManagerGo/NodeManagerGo /root/quorum-maker/.
 cd /root/go/src/
