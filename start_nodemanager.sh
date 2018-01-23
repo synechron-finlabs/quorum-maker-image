56cbf410aa5ba5d07ab3e35abb0bec2e3f4aaa89
Rajarshi Banerjee <rajarshi.banerjee1@synechron.com>
2018-01-23 08:49:58 +0000

    Update start_nodemanager.sh

--- start_nodemanager.sh
+++ start_nodemanager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+# $1 is rpcPort and $2 is golistenPort
 cd /root/quorum-maker/
 java -jar nodeManager.jar
-socket="http://localhost:$rpcPort"
-./NodeManagerGo $socket $golistenPort
+socket="http://localhost:$1"
+./NodeManagerGo $socket $2
