58466cff9a4468d83f8e5a54e6b46702a6d2267c
Dhyan Raj <dhyan.raj@synechron.com>
2018-02-24 13:17:11 -0500

    Fixed issues with NodeManager serouce code existence

--- build_image.sh
+++ build_image.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+set -x
+if [ -f  Dockerfile.bak ]; then
+    cp -f Dockerfile.bak Dockerfile
+
+else
+	cp -f Dockerfile Dockerfile.bak
+fi
+
 if [ "$1" == "java" ]; then
+	echo "Using Java based Node Manager"
+	
 	sed -i.bak -e '11,14d' Dockerfile
 	sed -i.bak -e '5,10d' start_nodemanager.sh
+	
+	if [ ! -e  NodeManager ]; then
+		ln -s ../NodeManager NodeManager
+	fi
+	
 else
+	echo "Using Golang based Node Manager"
+
 	sed -i.bak -e '7,10d' Dockerfile
 	sed -i.bak -e '4d' start_nodemanager.sh
+	
+
+	if [ ! -e  NodeManagerGo ]; then
+		ln -s ../NodeManagerGo NodeManagerGo
+	fi
 fi
 
 echo "Please provide the image name"
