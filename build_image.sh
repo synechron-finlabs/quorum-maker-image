07b5cd928a25c0c78b613f54f47f6afc52de4951
Dhyan Raj <dhyan.raj@synechron.com>
2018-06-16 08:31:58 -0400

    Dynamic tag names

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 imagename="syneblock/quorum-maker"
-tagname="2.0.2_Dev"
+
+branch=$(git branch | grep \* | cut -d ' ' -f2-)
+
+if [ $branch = "development" ]; then
+    tagname="2.0.2_Dev"
+elif [ $branch = "master" ]; then
+    tagname="2.0.2"
+else
+        tagname="2.0.2_"$branch
+fi
 
 if [ ! -z "$1" ]; then
     imagename=$1
@@ -11,11 +20,13 @@ if [ ! -z "$2" ]; then
     tagname=$2
 fi
 
+dockername=$imagename":"$tagname
+echo "Building image, "$dockername"..."
+
 
 lib/install_quorum.sh
 lib/build_nodemanager.sh
 lib/build_ui.sh
 
-dockername=$imagename":"$tagname
-echo "The docker image name: "$dockername
+
 docker build --no-cache -t $dockername .
