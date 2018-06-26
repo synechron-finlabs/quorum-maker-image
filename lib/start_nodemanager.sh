5c3498b6b222b9a316fa04acf0d95da65b22a13c
Dhyan Raj <dhyan.raj@synechron.com>
2018-06-26 19:26:55 -0400

    Added IP address to be passed for Attach node

--- lib/start_nodemanager.sh
+++ lib/start_nodemanager.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# $1 is rpcPort and $2 is golistenPort and $3 is logPort
+# $1 is rpcPort and $2 is golistenPort and $3 is IP Address
 cd /root/quorum-maker/
 
-socket="http://localhost:$1"
+if [ -z "$3" ]; then
+	socket="http://localhost:$1"
+else	
+	socket="http://$3:$1"
+fi
+
 
 if [ $# -gt 1 ]; then
-	./NodeManager $socket $2 $3
+	./NodeManager $socket $2
 else
 	./NodeManager
 fi
