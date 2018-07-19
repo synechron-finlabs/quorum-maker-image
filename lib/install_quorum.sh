4c2685ac454b1d45a30551ff79e2277ddbf9f5ae
rjbanner <rajarshibanerjee1112907@gmail.com>
2018-07-19 19:19:29 +0530

    Fix added for increasing the minimum gas limit for the network on Quorum 2.0.2

--- lib/install_quorum.sh
+++ lib/install_quorum.sh
@@ -9,6 +9,12 @@ rm -rf quorum
 
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://github.com/jpmorganchase/quorum.git
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/quorum alpine/git checkout $quorum_version
+cd $(pwd)/quorum
+sed -i 's/GasLimitBoundDivisor   = big.NewInt(1024)/GasLimitBoundDivisor   = big.NewInt(4096)/' params/protocol_params.go
+sed -i 's/MinGasLimit            = big.NewInt(5000)/MinGasLimit            = big.NewInt(700000000)/' params/protocol_params.go
+sed -i 's/GenesisGasLimit        = big.NewInt(4712388)/GenesisGasLimit        = big.NewInt(800000000)/' params/protocol_params.go
+sed -i 's/QuorumVersionPatch = 1/QuorumVersionPatch = 2/' params/version.go
+cd ..
 docker run -it --rm -v $(pwd)/quorum:/quorum -w /quorum golang:1.10.2 make all
 
 ## Change the owneship of quorum directory
