1e0b7aa0b1684b4803fa46eccd9c0901ab4888ab
Dhyan Raj <dhyanraj@gmail.com>
2019-02-11 21:46:59 -0500

    Upgraded to Tessera 0.8

--- lib/install_tessera.sh
+++ lib/install_tessera.sh
@@ -13,13 +13,15 @@ docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git -w /git/tessera alpine/git c
 chownDir 'tessera'
 sed -i "s;HASH LONGVARBINARY NOT NULL UNIQUE,;HASH LONGVARBINARY NOT NULL UNIQUE, TIMESTAMP BIGINT,;" tessera/data-migration/src/main/java/com/quorum/tessera/data/migration/H2DataExporter.java
 
+sed -i "s#\(public class\)#import javax.xml.bind.annotation.XmlAccessType;\nimport javax.xml.bind.annotation.XmlAccessorType;\n@XmlAccessorType\(XmlAccessType.FIELD\)\n\1#" tessera/config/src/main/java/com/quorum/tessera/config/HashicorpKeyVaultConfig.java
+
 docker run -it --rm -v $(pwd)/tessera:/tessera -w /tessera maven:3.6.0-jdk-8-slim /bin/sh -c 'mvn install -DskipTests -Djacoco.skip=true'
 
 ## Change the owneship of tessera directory
 chownDir 'tessera'
 
-cp tessera/tessera-app/target/tessera-app-${tessera_version}-app.jar tessera/tessera-app/target/tessera-app.jar
-cp tessera/data-migration/target/data-migration-${tessera_version}-cli.jar tessera/data-migration/target/data-migration-cli.jar
-cp tessera/config-migration/target/config-migration-${tessera_version}-cli.jar tessera/config-migration/target/config-migration-cli.jar
+cp tessera/tessera-app/target/tessera-app-*-app.jar tessera/tessera-app/target/tessera-app.jar
+cp tessera/data-migration/target/data-migration-*-cli.jar tessera/data-migration/target/data-migration-cli.jar
+cp tessera/config-migration/target/config-migration-*-cli.jar tessera/config-migration/target/config-migration-cli.jar
 
 echo $CYAN"Building Tessera, "$tessera_version" Completed."$COLOR_END
